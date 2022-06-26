535aaeec244455875d4226b0a4243a9ab60dfe2c
Rolexero <rotimiolaolu@gmail.com>
2022-06-26 13:21:00 +0100

    feat: :sparkles: add a p tag

--- src/Components/Wordle.js
+++ src/Components/Wordle.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Wordle = ({ data }) => {
-    const randomWords = data && data[Math.floor(Math.random() * data.length)]
-    return <div > { randomWords } < /div>;
+const Wordle = ({data}) => {
+          const randomWords = data && data[Math.floor(Math.random() * data.length)]
+  return (<div>{randomWords}
+  
+  <p>New one</p>
+  
+  </div>);
 }
 
 export default Wordle
