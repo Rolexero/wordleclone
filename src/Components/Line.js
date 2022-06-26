5233e4692ee8433e110e43396561d2a5cbb7817a
Rolexero <rotimiolaolu@gmail.com>
2022-06-26 18:25:52 +0100

    feat: :zap: add the keyup handler

---
+++ src/Components/Line.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Line = ({ guess, isFinal, data }) => {
+  let tiles = [];
+  let wordsLength = 5;
+  let className = 'tile'
+
+  for (let i = 0; i < wordsLength; i++) {
+    const element = guess[i];
+        if (isFinal) {
+      if (element === data[i]) {
+        className += ' correct'
+      }else if (data.includes(element)) {
+                className += " close";
+      }else{
+                        className += " incorrect";
+      }
+    }
+
+    tiles.push(
+      <div key={i} className={className}>
+        {element}
+      </div>
+    );
+  }
+  return <div className="line">{tiles}</div>;
+};
+
+export default Line;
--- src/Components/Wordle.js
+++ src/Components/Wordle.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Line from "./Line";
 
-const Wordle = ({data}) => {
-          const randomWords = data && data[Math.floor(Math.random() * data.length)]
-  return (<div>{randomWords}
-  
-  <p>New one</p>
-  
-  </div>);
-}
+const Wordle = ({ data }) => {
+  const randomWords = data && data[Math.floor(Math.random() * data.length)];
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
+  const [currentGuess, setCurrentGuess] = useState('')
+  const [gameIsOver, setGameIsOver] = useState(false)
 
-export default Wordle
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (gameIsOver) {
+        return;
+      }
+      if (e.key === 'Enter') {
+        if (currentGuess.length !== 5) {
+          return;
+        }
+        const isCorrect = data === currentGuess;
+        if (isCorrect) {
+            setGameIsOver(true);
+        }
+      }
+
+      if (e.key === "Backspace") {
+        setCurrentGuess(currentGuess=>currentGuess.slice(0, -1))
+        return;
+      }
+            if (currentGuess.length >= 5) {
+              return;
+            }
+
+      setCurrentGuess((prevGuess)=>prevGuess + e.key)
+
+    };
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [currentGuess, data, gameIsOver]);
+
+  return (
+    <div className="board">
+      {guesses.map((guess, i) => {
+        const isCurrentGuess = i === guesses.findIndex(val=>val === null)
+        return <Line guess={isCurrentGuess ? currentGuess : ""} key={i} isFinal={!isCurrentGuess && guess != null} data={data}/>;
+      })}
+    </div>
+  );
+};
+
+export default Wordle;
